b1d3c683990052560e933c8c53a3347218fb6559
classita2661 <classita.my@gmail.com>
2025-07-10 01:16:59 +0000

    chbapp

---
+++ chbcustom/chbcustom/doctype/chb/chb.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CHB and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CHB", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ chbcustom/chbcustom/doctype/chb/test_chb.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, CHB and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCHB(FrappeTestCase):
+	pass
